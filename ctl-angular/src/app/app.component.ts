eaef47dfbc10897539aee31ed4212ddc0316edcc
Diogo Silva <diogosilva212@gmail.com>
2025-01-19 19:59:52 +0000

    navbar component created

--- ctl-angular/src/app/app.component.ts
+++ ctl-angular/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import {Component} from '@angular/core';
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [NavbarComponent],
   standalone: true
 })
 export class AppComponent {
