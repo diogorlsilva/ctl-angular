ab84a396b1a0403ab225326d9660689e2ff15957
Diogo Silva <diogo.silva@criticalsoftware.com>
2025-10-16 11:45:43 +0100

    hash location workaround

--- ctl-angular/src/main.ts
+++ ctl-angular/src/main.ts
@@ -1,13 +1,15 @@
-import {bootstrapApplication} from '@angular/platform-browser';
-import {AppComponent} from './app/app.component';
-import {provideHttpClient} from "@angular/common/http";
-import {provideRouter} from "@angular/router";
-import {routes} from "./app/utils/routes.model";
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideHttpClient } from "@angular/common/http";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app/utils/routes.model";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    provideRouter(routes)
-  ],
+    providers: [
+        provideHttpClient(),
+        provideRouter(routes),
+        { provide: LocationStrategy, useClass: HashLocationStrategy }
+    ],
 })
-  .catch((err) => console.error(err));
+    .catch((err) => console.error(err));
